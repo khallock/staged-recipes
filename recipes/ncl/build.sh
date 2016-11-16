6be21690272f51c2373465dc0fa961ee2861f2f1
Kevin Hallock <hallock@ucar.edu>
2016-11-16 14:09:02 -0700

    Updates

--- recipes/ncl/build.sh
+++ recipes/ncl/build.sh
@@ -25,9 +25,6 @@ mkdir triangle_tmp && cd triangle_tmp && curl -q http://www.netlib.org/voronoi/t
 # add "-std=c99" to compile config files -- not needed after NCL 6.3.0
 sed -e "s/^\(#define CcOptions.*\)$/\1 -std=c99/" -i.backup "${conf_file}"
 
-# edit version string of NCL binary
-sed -e "s/^\(#define Nc.*Version\).*$/\1 ${PKG_VERSION}/" -e "s/^\(#define       EEMDlib   -lgsl\).*$/\1 -lgslcblas/" -i.backup config/Project && rm config/Project.backup
-
 echo "/*
  *  This file was created by the Configure script.
  */
